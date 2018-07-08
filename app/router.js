27a0947e54d7cd078f3c1ba69ab6ab741d9b02e0
Gfirey <pashkun73@gmai.com>
2018-07-08 04:57:11 +0300

    Initial commit

---
+++ app/router.js
@@ -0,0 +1,12 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'eeerock/config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+Router.map(function () {
+  this.route('bands');
+  this.route('songs');
+});
+export default Router;
